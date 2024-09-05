66bedfc043e14100eba71d015dbaca5c0ddde446
darshilchovatiya <119028616+darshilchovatiya@users.noreply.github.com>
2024-09-05 10:35:48 +0530

    Update DashboardTitle.js

--- Components/Dashboard/DashboardTitle/DashboardTitle.js
+++ Components/Dashboard/DashboardTitle/DashboardTitle.js
@@ -5,7 +5,7 @@ export default function DashboardTitle() {
     <div className="row">
     <div className="col-xl-12">
       <div class="sc-fb9f1326-0 TnemY d-sm-flex align-items-center justify-content-between">
-        <h6 class="sc-fb9f1326-1 izPFRb mb-sm-0">darshil patel</h6>
+        <h6 class="sc-fb9f1326-1 izPFRb mb-sm-0">DASHBOARD</h6>
         <div class="sc-fb9f1326-2 jncgKT page-title-right">
           <ol class="breadcrumb m-0">
             <li class="breadcrumb-item"></li>
