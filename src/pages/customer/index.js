d8ec7847e8b888d4e4bb75728a0823878d978553
darshilchovatiya <darshilchovatiya550@gmail.com>
2024-09-05 15:04:18 +0530

    titke

--- src/pages/customer/index.js
+++ src/pages/customer/index.js
@@ -1,13 +1,11 @@
 import CustomerMainCard from "../../../Components/Customer/CustmorCard/CustomerMainCard";
 import CustomerTable from "../../../Components/Customer/CustomerTable/CustomerTable";
-import CustomerTitle from "../../../Components/Customer/CustomerTitile/CustomerTitle";
 import LayoutMain from "../../../Components/LayoutMain/LayoutMain";
 
 export default function customer() {
   return (
     <>
       <LayoutMain>
-        <CustomerTitle/>
         <CustomerMainCard/>
         <CustomerTable />
       </LayoutMain> 
